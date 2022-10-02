3d3d1312a16b48157ac211cdbf00f0279b61c281
rariverasuper <67770958+rariverar@users.noreply.github.com>
2022-10-02 14:37:35 -0500

    cors añadido

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ import express from 'express';
 import employeesRoutes from './routes/employees.routes.js'
 import indexRoutes from './routes/index.routes.js'
 
+import cors from "cors"
 
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 app.use('/api', employeesRoutes)
